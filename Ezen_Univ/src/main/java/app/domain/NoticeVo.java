ced313150e1e9a15dd80577ce63bd64576e22cf3
eogml0404 <eogml0404@naver.com>
2023-11-09 16:44:48 +0900

    notice part

---
+++ Ezen_Univ/src/main/java/app/domain/NoticeVo.java
@@ -0,0 +1,77 @@
+package app.domain;
+
+public class NoticeVo {
+	private int nidx;
+	private String n_skipdate;
+	private String n_category;
+	private String n_contents;
+	private String n_writeday;
+	private int n_count;
+	private int pidx;
+	private int cidx;
+	private String n_delyn;
+	private String subject;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getNidx() {
+		return nidx;
+	}
+	public void setNidx(int nidx) {
+		this.nidx = nidx;
+	}
+	public String getN_skipdate() {
+		return n_skipdate;
+	}
+	public void setN_skipdate(String n_skipdate) {
+		this.n_skipdate = n_skipdate;
+	}
+	public String getN_category() {
+		return n_category;
+	}
+	public void setN_category(String n_category) {
+		this.n_category = n_category;
+	}
+	public String getN_contents() {
+		return n_contents;
+	}
+	public void setN_contents(String n_contents) {
+		this.n_contents = n_contents;
+	}
+	public String getN_writeday() {
+		return n_writeday;
+	}
+	public void setN_writeday(String n_writeday) {
+		this.n_writeday = n_writeday;
+	}
+	public int getN_count() {
+		return n_count;
+	}
+	public void setN_count(int n_count) {
+		this.n_count = n_count;
+	}
+	public int getPidx() {
+		return pidx;
+	}
+	public void setPidx(int pidx) {
+		this.pidx = pidx;
+	}
+	public int getCidx() {
+		return cidx;
+	}
+	public void setCidx(int cidx) {
+		this.cidx = cidx;
+	}
+	public String getN_delyn() {
+		return n_delyn;
+	}
+	public void setN_delyn(String n_delyn) {
+		this.n_delyn = n_delyn;
+	}
+
+
+}
