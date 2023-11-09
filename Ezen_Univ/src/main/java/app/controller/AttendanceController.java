2d930ecfcd55ca2b33da987643c5215543694fdb
Minjung Kim <mjkspace@gmail.com>
2023-11-09 16:56:27 +0900

    attendanceSituation commit

--- Ezen_Univ/src/main/java/app/controller/AttendanceController.java
+++ Ezen_Univ/src/main/java/app/controller/AttendanceController.java
@@ -6,37 +6,46 @@ import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import app.dao.AttendanceDao;
 import app.domain.AttendanceVo;
 
 @WebServlet("/AttendanceController")
-public class AttendanceController {
+public class AttendanceController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-
+	
 	private String location;
 	public AttendanceController(String location) {
 		this.location = location;
 	}
-
+	
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		if (location.equals("studentCourseList.do")) {
-
+		
+		if(location.equals("attendanceSituation_s.do")) {
+			System.out.println("컨트롤러");
 			AttendanceDao ad = new AttendanceDao();
-			ArrayList<AttendanceVo> clist = ad.studentCourseList();
-
-			//화면으로 가지고 간다
-			request.setAttribute("clist", clist);	
-
-			String path ="/attendance/attendanceSituation_s.jsp";
-			//화면용도의 주소는 포워드로 토스해서 해당 찐주소로 보낸다
+			
+			HttpSession session = request.getSession();
+			int sidx = ((Integer)(session.getAttribute("sidx"))).intValue();
+			
+			
+			ArrayList<AttendanceVo> list = ad.attendanceCourseList(sidx);
+			
+			
+			request.setAttribute("list", list);
+			
+			String path = "/attendance/attendanceSituation_s.jsp";
+			//화면용도의 주소는 forward로 토스해서 해당 찐 주소로 보낸다.
 			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);	
-
-			}
-
+			rd.forward(request, response);
+			
+		}
 	}
+	
 }
